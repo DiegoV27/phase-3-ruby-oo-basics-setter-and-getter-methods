30f13c56e4d57417a402d1e5bafed913dde6fd88
Diego Valero <diego27jay@gmail.com>
2022-02-24 09:57:47 -0700

    commit tree b87d91f8decfee797898c8f59b659ad0292b14bd

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
+require 'pry'
 class Dog
+    attr_reader :name
+    attr_accessor :breed 
 
+
+    def initialize(name, breed = "lab")
+        @name = name
+        @breed = breed 
+    
+    
+    end
 end
+
+d1 = Dog.new("charlie")
+binding.pry
